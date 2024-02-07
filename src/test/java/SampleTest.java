39a855610d5878e5f06b7f0891f620715fb389eb
Ankursh287 <ankursh287@gmail.com>
2024-02-07 16:36:08 +0530

    Updated

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,10 +2,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SampleTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chromedriver", "C:\\Users\\ankur\\Documents\\Code\\Selenium\\libs\\drivers\\chrome-win64\\chrome.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://chromedriver.chromium.org/home");
+        Thread.sleep(2000);
         driver.close();
     }
 }
