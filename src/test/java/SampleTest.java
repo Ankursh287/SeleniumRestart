68e43ebd7c8473c8dc755726ad653d201e59bd37
Ankursh287 <ankursh287@gmail.com>
2024-02-07 16:36:08 +0530

    Updated

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,11 +1,19 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SampleTest {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chromedriver", "C:\\Users\\ankur\\Documents\\Code\\Selenium\\libs\\drivers\\chrome-win64\\chrome.exe");
+    public static void main(String[] args) throws InterruptedException {
+       // System.setProperty("webdriver.chromedriver", "C:\\Users\\ankur\\Documents\\Code\\Selenium\\libs\\drivers\\chrome-win64\\chrome.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://chromedriver.chromium.org/home");
+        Thread.sleep(2000);
+        driver.get("https://www.letskodeit.com/");
+        driver.findElement(By.xpath("//a[@href='/login']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.partialLinkText("PRACTICE")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@href='/practice']")).click();
         driver.close();
     }
 }
