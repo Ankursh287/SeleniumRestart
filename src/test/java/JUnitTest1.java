1d0fb03aa5a10e4aaebf68e797ade530fe2d18eb
ankursh287 <ankursh287@gmail.com>
2024-02-18 19:01:38 +0530

    Updated

--- src/test/java/JUnitTest1.java
+++ src/test/java/JUnitTest1.java
@@ -3,25 +3,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.*;
-
-public class JUnitTest {
+public class JUnitTest1 {
     static WebDriver driver;
+
     @BeforeClass
     public static void setupBeforeClass() throws Exception {
-        driver = new ChromeDriver();
-        driver.get("https://chromedriver.chromium.org/home");
-        Thread.sleep(2000);
-        driver.get("https://www.letskodeit.com/");
+        System.out.println("Executed before Class..");
     }
 
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
         System.out.println("Executed after Class..");
-        driver.quit();
     }
 
-/*    @Before
+    @Before
     public void setUp() throws Exception {
         System.out.println("Executed before Method..");
     }
@@ -29,20 +24,15 @@ public class JUnitTest {
     @After
     public void tearDown() throws Exception {
         System.out.println("Executed after Method..");
-    }*/
+    }
 
     @Test
     public void test1() throws Exception {
         System.out.println("Test Method#1");
-        driver.findElement(By.xpath("//a[@href='/login']")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.partialLinkText("PRACTICE")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//a[@href='/practice']")).click();
     }
 
-/*    @Test
+    @Test
     public void test2() throws Exception {
         System.out.println("Test Method#1");
-    }*/
+    }
 }
---
+++ src/test/java/JUnitTest2.java
@@ -0,0 +1,50 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JUnitTest2 {
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setupBeforeClass() throws Exception {
+        driver = new ChromeDriver();
+        driver.manage().window().fullscreen();
+        //driver.get("https://chromedriver.chromium.org/home");
+        Thread.sleep(2000);
+        driver.get("https://www.letskodeit.com/");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        System.out.println("Executed after Class..");
+        driver.quit();
+    }
+
+/*    @Before
+    public void setUp() throws Exception {
+        System.out.println("Executed before Method..");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("Executed after Method..");
+    }*/
+
+    @Test
+    public void test1() throws Exception {
+        System.out.println("Test Method#1");
+        driver.findElement(By.xpath("//a[@href='/login']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.partialLinkText("PRACTICE")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@href='/practice']")).click();
+    }
+
+/*    @Test
+    public void test2() throws Exception {
+        System.out.println("Test Method#1");
+    }*/
+}
