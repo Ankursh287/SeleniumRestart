e13dbe609cdffcb504efc6be3109f30fb2083505
ankursh287 <ankursh287@gmail.com>
2024-03-05 18:20:24 +0530

    Updated

--- src/test/java/testngCheck/ParallelTestWithMultipleBrowser.java
+++ src/test/java/testngCheck/ParallelTestWithMultipleBrowser.java
@@ -5,13 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 @Test()
-public class SampleTest1 {
+public class ParallelTestWithMultipleBrowser {
     WebDriver driver;
     String baseURL;
 
