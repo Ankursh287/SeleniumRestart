29c1e0f8e25252f428e6dfdac67284a9cf52c029
ankursh287 <ankursh287@gmail.com>
2024-03-07 13:49:45 +0530

    Updated

--- src/test/java/testngCheck/TestNG_ITestResultDemo.java
+++ src/test/java/testngCheck/TestNG_ITestResultDemo.java
@@ -5,44 +5,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestNG_ITestResultDemo {
-   /* WebDriver driver;
-    String baseURL;
 
-    @BeforeTest
-    public void setUp(String browser) {
-        baseURL = "https://www.letskodeit.com/home";
-        switch (browser) {
-            case "Chrome" -> driver = new ChromeDriver();
-            case "Firefox" -> driver = new FirefoxDriver();
-        }
-        driver.get(baseURL);
+    @Test
+    public void test1() {
+        System.out.println("Running -> testMethod1");
+        Assert.assertTrue(false);
     }
 
     @Test
-    public void test() {
-        WebElement loginLink = driver.findElement(By.xpath("//a[@href='/login']"));
-        loginLink.click();
-        WebElement emailField = driver.findElement(By.xpath("//input[@placeholder='Email Address']"));
-        WebElement passwordField = driver.findElement(By.xpath("//input[@placeholder='Password']"));
-        WebElement loginButton = driver.findElement(By.cssSelector("#login"));
-        emailField.sendKeys("test@email.com");
-        passwordField.sendKeys("password");
-        loginButton.click();
-        // Assert.assertTrue(false);
-    }*/
-    @Test
+    public void test2() {
+        System.out.println("Running -> testMethod2");
+        Assert.assertTrue(true);
+    }
 
-    @AfterTest
-    public void tearDown(/*ITestResult testResult*/) {
-/*        if (!(testResult.isSuccess())){
-            System.out.println("Test was not successful");
-        }*/
-        // System.out.println(testResult.getMethod().getMethodName());
-       // driver.quit();
+    @AfterMethod
+    public void tearDown(ITestResult testResult) {
+      if (!(testResult.isSuccess())) {
+            System.out.println("Reporting via isSuccess() method --> Test was not successful ");
+        }
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            System.out.println("Reporting FAILURE via getSuccess method --> Test was not successful " + testResult.getMethod().getMethodName());
+        }
+        if (testResult.getStatus() == ITestResult.SUCCESS) {
+            System.out.println("Reporting SUCCESS via getSuccess method --> Test was not successful " + testResult.getName());
+        }
     }
 }
