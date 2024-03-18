68a774b50d445abc3605a84e4ad85068066f5006
ankursh287 <ankursh287@gmail.com>
2024-03-18 21:46:27 +0530

    Updated

--- src/test/java/DataDriven/SampleTestNG.java
+++ src/test/java/DataDriven/SampleTestNG.java
@@ -1,6 +1,5 @@
 package DataDriven;
 
-import Utils.ExcelUtility;
 import com.aventstack.extentreports.ExtentReports;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,24 +8,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.List;
 
 public class SampleTestNG {
     WebDriver driver;
 
-    @BeforeTest
-    public void setUp(/*String browser*/) {
+    @BeforeClass
+    public void setUp(/*String browser*/) throws InterruptedException {
 /*        switch (browser) {
             case "Chrome" -> driver = new ChromeDriver();
             case "Firefox" -> driver = new FirefoxDriver();
         }*/
         driver = new ChromeDriver();
         driver.get(Constants.URL);
+        WebElement loginLink = driver.findElement(By.xpath("//a[@href='/login']"));
+        loginLink.click();
+        Thread.sleep(2000);
         ExcelUtility.setExcelFile(Constants.File_Path + Constants.File_Name, "LoginTests");
     }
 
@@ -38,11 +37,6 @@ public class SampleTestNG {
 
     @Test(dataProvider = "loginData")
     public void testUsingExcel(String userName, String password) throws Exception {
-        WebElement loginLink = driver.findElement(By.xpath("//a[@href='/login']"));
-        loginLink.click();
-
-        Thread.sleep(2000);
-
         WebElement emailField = driver.findElement(By.xpath("//input[@placeholder='Email Address']"));
         WebElement passwordField = driver.findElement(By.xpath("//input[@placeholder='Password']"));
         WebElement loginButton = driver.findElement(By.cssSelector("#login"));
@@ -69,12 +63,12 @@ public class SampleTestNG {
         loginButton.click();
     }*/
 
-    @AfterTest
+    @AfterClass
     public void tearDown(ITestResult testResult) {
-        if (!(testResult.isSuccess())) {
+/*        if (!(testResult.isSuccess())) {
             System.out.println("Test was not successful");
         }
-        System.out.println(testResult.getMethod().getMethodName());
+        System.out.println(testResult.getMethod().getMethodName());*/
         driver.quit();
     }
 }
